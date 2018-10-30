a72c5805a4d390b25abe8f9b6beb5f86c2c36704
concerto <562725262@qq.com>
2018-10-30 16:46:26 +0800

    DOM操作版未加速度

--- script/script.js
+++ script/script.js
@@ -1,45 +1,31 @@
-window.onload=function(){
-	if(window.orientation != 0){
-		alert("请竖屏显示");
-		history.go(0);
-	}
-};
-//上面那些是一个强制竖屏（？）的玩意儿，用户体验极差的那种23333
+var ball   = document.querySelector('.ball');
+var garden = document.querySelector('.garden');
+var output = document.querySelector('.output');
 
-if (window.DeviceOrientationEvent) {
-	window.addEventListener('deviceorientation', DeviceOrientationHandler, false);
-} else {
-	alert("您的浏览器不支持DeviceOrientation");
-};
-function DeviceOrientationHandler(event){
-      document.getElementById("alpha").innerHTML = event.alpha;
-      document.getElementById("beta").innerHTML = event.beta;
-      document.getElementById("gamma").innerHTML = event.gamma;
-      document.getElementById("heading").innerHTML = event.webkitCompassHeading;
-      document.getElementById("accuracy").innerHTML = event.webkitCompassAccuracy;
+var maxX = garden.clientWidth  - ball.clientWidth;
+var maxY = garden.clientHeight - ball.clientHeight;
+
+function handleOrientation(event) {
+  var x = event.beta;  // In degree in the range [-180,180]
+  var y = event.gamma; // In degree in the range [-90,90]
+
+  output.innerHTML  = "beta : " + x + "\n";
+  output.innerHTML += "gamma: " + y + "\n";
+
+  // Because we don't want to have the device upside down
+  // We constrain the x value to the range [-90,90]
+  if (x >  90) { x =  90};
+  if (x < -90) { x = -90};
+
+  // To make computation easier we shift the range of 
+  // x and y to [0,180]
+  x += 90;
+  y += 90;
+
+  // 10 is half the size of the ball
+  // It center the positioning point to the center of the ball
+  ball.style.top  = (maxX*x/180 - 10) + "px";
+  ball.style.left = (maxY*y/180 - 10) + "px";
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+window.addEventListener('deviceorientation', handleOrientation);
