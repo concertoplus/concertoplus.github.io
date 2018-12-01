c471750ae979512bfe118e2ac0d4cd9d38e68551
concerto <562725262@qq.com>
2018-12-01 14:43:14 +0800

    test

---
+++ script/script.js
@@ -0,0 +1,247 @@
+icon.onclick = function () {
+	cover.style.display="none";
+	page1.style.display="block";
+}
+icon011.onclick = function () {
+	page1.style.display="none";
+	page2.style.display="block";
+}
+icon021.onclick = function () {
+	page2.style.display="none";
+	page4.style.display="block";
+}
+icon022.onclick = function () {
+	page2.style.display="none";
+	page3.style.display="block";
+}
+icon023.onclick = function () {
+	page2.style.display="none";
+	page6.style.display="block";
+}
+icon024.onclick = function () {
+	page2.style.display="none";
+	page5.style.display="block";
+}
+icon025.onclick = function () {
+	page2.style.display="none";
+	page7.style.display="block";
+}
+icon026.onclick = function () {
+	function part2(){
+		text081.style.display="none"
+		icon081.style.display="block"
+		icon082.style.display="block"
+		icon083.style.display="block"
+		icon084.style.display="block"
+		icon085.style.display="block"
+	}
+	page2.style.display="none";
+	page8.style.display="block";
+	setTimeout(part2,3000)
+}
+icon032.onclick = function () {
+	page3.style.display="none";
+	page2.style.display="block";
+}
+icon033.onclick = function () {
+	page3.style.display="none";
+	page2.style.display="block";
+}
+icon042.onclick = function () {
+	page4.style.display="none";
+	page2.style.display="block";
+}
+icon043.onclick = function () {
+	page4.style.display="none";
+	page2.style.display="block";
+}
+icon052.onclick = function () {
+	page5.style.display="none";
+	page2.style.display="block";
+}
+icon053.onclick = function () {
+	page5.style.display="none";
+	page2.style.display="block";
+}
+icon062.onclick = function () {
+	page6.style.display="none";
+	page2.style.display="block";
+}
+icon033.onclick = function () {
+	page6.style.display="none";
+	page2.style.display="block";
+}
+icon072.onclick = function () {
+	page7.style.display="none";
+	page2.style.display="block";
+}
+icon073.onclick = function () {
+	page7.style.display="none";
+	page2.style.display="block";
+}
+icon081.onclick = function () {
+	page8.style.display="none";
+	page9.style.display="block";
+	text091.style.display="block"
+}
+icon082.onclick = function () {
+	page8.style.display="none";
+	page9.style.display="block";
+	text092.style.display="block"
+}
+icon083.onclick = function () {
+	page8.style.display="none";
+	page9.style.display="block";
+	text093.style.display="block"
+}
+icon084.onclick = function () {
+	page8.style.display="none";
+	page9.style.display="block";
+	text094.style.display="block"
+}
+icon085.onclick = function () {
+	page8.style.display="none";
+	page9.style.display="block";
+	text095.style.display="block"
+}
+page9.onclick = function () {
+	page9.style.display="none";
+	page10.style.display="block";
+	text091.style.display="none"
+	text092.style.display="none"
+	text093.style.display="none"
+	text094.style.display="none"
+	text095.style.display="none"
+}
+icon102.onclick = function () {
+	page10.style.display="none";
+	page11.style.display="block";
+}
+text101.onclick = function () {
+	page10.style.display="none";
+	page11.style.display="block";
+}
+icon103.onclick = function () {
+	page10.style.display="none";
+	page12.style.display="block";
+}
+text102.onclick = function () {
+	page10.style.display="none";
+	page12.style.display="block";
+}
+icon111.onclick = function () {
+	page11.style.display="none";
+	cover.style.display="block";
+}
+icon122.onclick = function () {
+	page12.style.display="none";
+	page13.style.display="block";
+}
+text121.onclick = function () {
+	page12.style.display="none";
+	page13.style.display="block";
+}
+icon123.onclick = function () {
+	page12.style.display="none";
+	page14.style.display="block";
+}
+text122.onclick = function () {
+	page12.style.display="none";
+	page14.style.display="block";
+}
+icon124.onclick = function () {
+	page12.style.display="none";
+	page17.style.display="block";
+}
+text123.onclick = function () {
+	page12.style.display="none";
+	page17.style.display="block";
+}
+icon132.onclick = function () {
+	page13.style.display="none";
+	cover.style.display="block";
+}
+text131.onclick = function () {
+	page13.style.display="none";
+	cover.style.display="block";
+}
+icon142.onclick = function () {
+	page14.style.display="none";
+	page15.style.display="block";
+}
+text141.onclick = function () {
+	page14.style.display="none";
+	page15.style.display="block";
+}
+icon143.onclick = function () {
+	page14.style.display="none";
+	page15.style.display="block";
+}
+text142.onclick = function () {
+	page14.style.display="none";
+	page15.style.display="block";
+}
+icon144.onclick = function () {
+	page14.style.display="none";
+	page15.style.display="block";
+}
+text143.onclick = function () {
+	page14.style.display="none";
+	page15.style.display="block";
+}
+icon152.onclick = function () {
+	page15.style.display="none";
+	page16.style.display="block";
+}
+text151.onclick = function () {
+	page15.style.display="none";
+	page16.style.display="block";
+}
+icon161.onclick = function () {
+	page16.style.display="none";
+	cover.style.display="block";
+}
+icon172.onclick = function () {
+	page17.style.display="none";
+	page18.style.display="block";
+}
+text171.onclick = function () {
+	page17.style.display="none";
+	page18.style.display="block";
+}
+icon173.onclick = function () {
+	page17.style.display="none";
+	page18.style.display="block";
+}
+text172.onclick = function () {
+	page17.style.display="none";
+	page18.style.display="block";
+}
+icon174.onclick = function () {
+	page17.style.display="none";
+	page19.style.display="block";
+}
+text173.onclick = function () {
+	page17.style.display="none";
+	page19.style.display="block";
+}
+icon182.onclick = function () {
+	page18.style.display="none";
+	cover.style.display="block";
+}
+text181.onclick = function () {
+	page18.style.display="none";
+	cover.style.display="block";
+}
+icon192.onclick = function () {
+	page19.style.display="none";
+	cover.style.display="block";
+}
+text191.onclick = function () {
+	page19.style.display="none";
+	page20.style.display="block";
+}
+icon202.onclick = function () {
+	page20.style.display="none";
+	cover.style.display="block";
+}
